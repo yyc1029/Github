c136291a3faa59e8d58987c4a33a5e1a95690790
yyc1029 <austinyin9201@gmail.com>
2025-03-26 16:22:29 +0800

    uva10783.cpp

--- CPE/uva10783.cpp
+++ CPE/uva10783.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main(){
     int times;
     cin >> times;
+    int numcase = 1;
     for(int i = 0; i < times; i++){
         int a, b;
-        int numcase = 1;
-        while(cin >> a >> b){
-            int result = 0;
-            for(int j = a; j <= b; j++){
-                if(j % 2 == 1){
-                    result += j;
-                }
+        cin >> a >> b;
+        int result = 0;
+        for(int j = a; j <= b; j++){
+            if(j % 2 == 1){
+                result += j;
             }
-            cout << "Case :" << numcase << ": " << result << endl;
-            numcase++;
         }
+        cout << "Case " << numcase << ": " << result << endl;
+        numcase++;
+        
     }
     return 0;
 }
