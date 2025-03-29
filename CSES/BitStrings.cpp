46a97c23831bc302dcd6df7b7fff4c3218072dbb
yyc1029 <austinyin9201@gmail.com>
2025-03-29 14:06:35 +0800

    accept

---
+++ CSES/BitStrings.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+const int mod = (int)(1e9+7);
+int main(){
+    ll n = 0;
+    ll ans = 1;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        ans *= 2;
+        ans %= mod;
+    }
+    cout << ans << endl;
+    return 0;
+}
