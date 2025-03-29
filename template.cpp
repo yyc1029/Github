e292ac0208339c91750c9d6418fbaabb15b0f38b
yyc1029 <austinyin9201@gmail.com>
2025-03-29 18:33:12 +0800

    upload template

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define opt ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+const int MAXN = 1e7 + 50;
+const int Mod = 1e9 + 7;
+
+int kcm(int base, int power){
+    int res = 1;
+    while(power > 0){
+        if(power & 1)res = res * base;
+        base *= base;
+        power >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    opt
+}
